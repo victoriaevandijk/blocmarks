5d6c3dcaae774bd8eeb4aed4db053b8ffaae919f
Torie <victoriaevandijk@gmail.com>
2016-03-10 04:59:30 +0000

    User Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :users, only: [:show] do
+        resources :items, only: [:create, :destroy]
+  end
+  
   get 'welcome/index'
 
   get 'welcome/about'
