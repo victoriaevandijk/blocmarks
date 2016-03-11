1d70e14f7be5a3c72f1683e1637ca154e40d6cf3
Torie <victoriaevandijk@gmail.com>
2016-03-11 01:09:24 +0000

    Bookmarks CRUD

---
+++ app/controllers/topics/bookmarks_controller.rb
@@ -0,0 +1,54 @@
+class Topics::BookmarksController < ApplicationController
+  def show
+  end
+
+  def new
+    #@topic = Topic.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+  
+  def create
+    #@topic = Topic.find(params[:id])
+     @topic = Topic.find(params[:topic_id])
+     @bookmark = Bookmark.new(params.require(:bookmark).permit(:url))
+     @bookmark.topic = @topic
+     if @bookmark.save
+       flash[:notice] = "Bookmark saved!"
+       redirect_to @topic
+     else
+       flash[:error] = "Error creating bookmark. Please try again."
+       render :new
+     end
+   end
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.save
+  end
+  
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+    
+    if @bookmark.update_attributes(params.require(:bookmark).permit(:url))
+      flash[:notice] = "Bookmark was updated."
+      redirect_to [@topic]
+    else
+      flash[:error] = "There was an error saving the bookmark. Please try again."
+      render :edit
+    end
+  end
+  
+  def destroy
+     @bookmark = Bookmark.find(params[:id])
+ 
+     if @bookmark.destroy
+       flash[:notice] = "Bookmark was deleted successfully."
+       redirect_to topics_path
+     else
+       flash[:error] = "There was an error deleting the bookmark."
+       render :show
+     end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :topics 
+
+  resources :topics do
+    resources :bookmarks, except: [:index], controller: 'topics/bookmarks'
+  end
 
   devise_for :users
 
